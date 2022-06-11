102353b68cc1d19f94952a1edb6b63a6c35644f6
singhbikramjeet <105505396+singhbikramjeet@users.noreply.github.com>
2022-06-11 17:44:05 -0400

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+//Name: Bikramjeet Singh
+//Student No: 301203970
+//Date : 10-06-2022
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+module.exports = router;
